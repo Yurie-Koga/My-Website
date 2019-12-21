e3672c4e5c039dc02c9adb92d958b424c5ff3a65
Yurie <yurie.koga.a@gmail.com>
2019-12-20 19:28:38 -0800

    update homepage

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import { Route, Link, BrowserRouter as Router } from "react-router-dom";
-// import App from "./App";
-// import Home from "./pages/Home";
-
-// const routing = (
-//   <Router>
-//     {/* <header>Header</header> */}
-//     <div>
-//       <Route path="/" component={App} />
-//       <Route path="/home" component={Home} />
-//     </div>
-//     {/* <footer>Footer</footer> */}
-//   </Router>
-// );
-// ReactDOM.render(routing, document.getElementById("root"));
-
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,16 @@ export default function HomePage() {
   return (
     <div className="container">
       <h1>Home Page</h1>
-      <p>
-        <Link to="/taniarascia">taniarascia</Link> on GitHub.
-      </p>
+
+      <div class="hero-image">
+        <div class="hero-text">
+          <h1 style={{ fontSize: "50px" }}>I am Yurie Koga</h1>
+          <p>And I'm a Photographer</p>
+          <button>Hire me</button>
+        </div>
+      </div>
+
+      <p>Page Content..</p>
     </div>
   );
 }
