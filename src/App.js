d10e1a13ad1a5f73ff94fdd6241787b8059b6a9b
Yurie <yurie.koga.a@gmail.com>
2019-12-22 12:25:09 -0800

    update header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-// We will create these two pages in a moment
 import HomePage from "./pages/Home";
 import BlogPage from "./pages/Blog";
 import ContactPage from "./pages/Contact";
 
 export default function App() {
   return (
+    // error @App.test.js : don't use switch
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/blog" component={BlogPage} />
@@ -14,3 +14,18 @@ export default function App() {
     </Switch>
   );
 }
+
+// ============== Using BrowerRouter not work ==============
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route exact path="/" component={HomePage} />
+//         <Route path="/blog" component={BlogPage} />v
+//         <Route path="/contact" component={ContactPage} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
+// ============== Using BrowerRouter not work ==============
