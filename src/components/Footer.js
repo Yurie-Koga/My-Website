4971c8a531f0bd51c18b60e7a9ea81ac50c5911d
Yurie <yurie.koga.a@gmail.com>
2019-12-27 22:09:39 -0800

    delete extra comments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
     <footer class="footer">
       <p class="footerCopyRight">
         <small>
-          &copy; Copyright 2019 <span className="myName"> Yurie Koga </span> All
-          rights reserved.
+          &copy; 2019 <span className="myName"> Yurie Koga </span> All rights
+          reserved.
         </small>
       </p>
     </footer>
