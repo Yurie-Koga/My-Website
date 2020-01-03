9b8312c067da8454c29bb8037cb23b3dfcde6791
Yurie <yurie.koga.a@gmail.com>
2020-01-02 22:29:49 -0800

    update contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,14 +7,12 @@ export default function ContactPage() {
       <div className="parallax">
         <div className="text text-contact">
           <h1>Thank you for visiting!</h1>
-          <h1>Did you enjoy my website?</h1>
         </div>
       </div>
 
       <div className="coverImage">
         <div className="coverText">
           <h1>Contact</h1>
-          <p>If you have questions, I have answers.</p>
           <p>I would love to hear from you!</p>
           <p>BC, Canada</p>
           <IconLink IconName="LinkedIn" />
