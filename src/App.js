e1d7fa4904626b4cde22b72a97879264aedea3cf
Yurie <yurie.koga.a@gmail.com>
2020-01-21 10:52:23 -0800

    update App.js ro accept reload pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ContactPage from "./pages/Contact";
 import Header from "./components/Header";
@@ -7,11 +7,11 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/contact" component={ContactPage} />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
